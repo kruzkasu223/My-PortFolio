abaf9c6c929db6bda4ef41cc412e512045aca29b
Bhardwaj Thumber <50964723+BhardwajThummar@users.noreply.github.com>
2021-02-07 16:08:48 +0530

    Initial commit

---
+++ js/greet.js
@@ -0,0 +1,19 @@
+var welcome;
+var date = new Date();
+var hour = date.getHours();
+var minute = date.getMinutes();
+var second = date.getSeconds();
+if (minute < 10) {
+    minute = "0" + minute;
+}
+if (second < 10) {
+    second = "0" + second;
+}
+if (hour < 12) {
+    welcome = "Good Morning";
+} else if (hour < 17) {
+    welcome = "Good Afternoon";
+} else {
+    welcome = "Good Evening";
+}
+document.write(welcome + ',');
